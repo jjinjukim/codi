1949432bfa4e456294cbebaddabaee08de5f5268
jjinjukim <developer.do.de.gee@gmail.com>
2024-07-17 00:53:05 +0900

    initial commit

---
+++ src/test/java/com/musinsa/codi/CodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.codi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
